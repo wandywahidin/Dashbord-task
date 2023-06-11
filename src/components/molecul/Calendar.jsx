10c6a27ca8ffde80882cc85d17ce025c2961e919
wandy <wandywahidin53@gmail.com>
2023-06-11 16:13:42 +0700

    fix layout2

--- src/components/molecul/Calendar.jsx
+++ src/components/molecul/Calendar.jsx
@@ -6,7 +6,7 @@ import "react-datepicker/dist/react-datepicker.css";
 const Calendar = () => {
   const [startDate, setStartDate] = useState(new Date());
   return (
-    <div className="w-[318px] flex justify-center">
+    <div className="p-5 rounded-2xl shadow bg-[#F5F2FF] flex justify-center">
         <DatePicker
           selected={startDate}
           onChange={(date) => setStartDate(date)}
